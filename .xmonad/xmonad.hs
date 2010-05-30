0505f2b7a93134361d75d27ba875ec962802594f
Uwe <keksvernichter@gmail.com>
2010-05-30 20:30:39 +0200

    changed xmonad.hs .dzen_conkyrc

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -19,13 +19,14 @@ import XMonad.Actions.WindowGo
 import qualified XMonad.Actions.Search as S
 import XMonad.Actions.Search
 import qualified XMonad.Actions.Submap as SM
- 
+import XMonad.Actions.GridSelect
+
 -- utils
+import XMonad.Util.Scratchpad (scratchpadSpawnAction, scratchpadManageHook, scratchpadFilterOutWorkspace)
 import XMonad.Util.Run(spawnPipe)
 import qualified XMonad.Prompt as P
 import XMonad.Prompt.Shell
 import XMonad.Prompt
-import XMonad.Prompt.Workspace
  
 -- hooks
 import XMonad.Hooks.DynamicLog
@@ -41,19 +42,21 @@ import XMonad.Layout.IM
 import XMonad.Layout.Tabbed
 import XMonad.Layout.PerWorkspace (onWorkspace)
 import XMonad.Layout.Grid
- 
+
+-- Data.Ratio for IM Layout
+import Data.Ratio ((%))
+
 -- Main --
 main = do
-	d <- spawnPipe myStatusBar
-	spawn myOtherBar
-	xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig
-		{ layoutHook            = myLayoutHook
-		, manageHook            = myManageHook
+	xmproc <- spawnPipe "xmobar"
+	xmonad  $ withUrgencyHook NoUrgencyHook $ defaultConfig
+	  { manageHook            = myManageHook
+		, layoutHook            = myLayoutHook 
 		, borderWidth           = myBorderWidth
 		, normalBorderColor     = myNormalBorderColor
 		, focusedBorderColor    = myFocusedBorderColor
 		, keys                  = myKeys
-		, logHook               = myLogHook d
+		, logHook               = myLogHook xmproc
 		, modMask               = myModMask
 		, terminal              = myTerminal
 		, workspaces            = myWorkspaces
@@ -63,81 +66,60 @@ main = do
 -- hooks
 -- automaticly switching app to workspace
 myManageHook :: ManageHook
-myManageHook = composeAll . concat $ 
-                [ [ isFullscreen                      --> doFullFloat ]
-                , [ className =? a                    --> doCenterFloat | a <- myCenterFloats ] 
-                , [ moveToC "Gimp"                    "gimp" ]
-                , [ moveToC "Namoroka"                "web" ]
-                , [ moveToC "MPlayer"                 "vid" ]
-                , [ moveToC "VirtualBox"              "virtual" ]
-                , [ moveToC "Evince"                  "pdf" ] ]
-				++
-				[ [ resource  =? a --> doCenterFloat  | a <- myCenterFloats ] ]
-                where
-                    myIgnores                         = ["trayer"]
-                    myCenterFloats                    = ["Xmessage","Zenity","GQview","Pokerth"]
-                    myOtherFloats                     = []
-                    moveToC c w                       = className =? c --> doF (W.shift w)
- 
- 
--- dzen options are external except the font and the colors
-myFont="-*-terminus-*-r-normal-*-12-*-*-*-*-*-iso8859-*"
-myStatusBar = "dzen2 `cat ~/.dzen2_l` -fg '#606060' -bg '#303030' -e 'onexit=ungrabmouse'  -fn " ++ myFont
-myOtherBar = "conky -c ~/.dzen_conkyrc | dzen2 `cat ~/.dzen2_r` -fg '#606060' -bg '#303030' -fn " ++ myFont
+myManageHook = scratchpadManageHook (W.RationalRect 0.25 0.375 0.5 0.35) <+> ( composeAll . concat $
+                [ [ isFullscreen                      --> doFullFloat 
+                , className =? "OpenOffice.org"       --> doShift "5:doc"
+								, className =? "Xmessage"             --> doCenterFloat
+								, className =? "Zenity"               --> doCenterFloat
+                , className =? "feh"                  --> doCenterFloat
+                , className =? "Gimp"                 --> doShift "9:gimp"
+                , className =? "Namoroka"             --> doShift "2:web"
+                , className =? "MPlayer"              --> doShift "8:vid"
+                , className =? "VirtualBox"           --> doShift "6:vbox"
+                , className =? "Evince"               --> doShift "4:pdf"]
+
+								]
+																		) <+> manageDocks
 
 --logHook
 myLogHook :: Handle -> X ()
-myLogHook h = dynamicLogWithPP $ customPP
-	{ ppCurrent = dzenColor "#303030" "#909090" . pad
-	, ppHidden  = dzenColor "#909090" "" . pad
-	, ppLayout  = dzenColor "#909090" "" . pad
-	, ppUrgent  = wrap (dzenColor "#ff0000" "" "{") (dzenColor "#ff0000" "" "}") . pad
-	, ppTitle   = wrap "^fg(#909090)[ " " ]^fg()" . shorten 40 
-	, ppWsSep   = ""
-	, ppSep     = "  "
-	, ppOutput  = hPutStrLn h
-	}
+myLogHook h = dynamicLogWithPP $ customPP { ppOutput = hPutStrLn h }
 
 ---- Looks --
 ---- bar
 customPP :: PP
 customPP = defaultPP
-	{ ppHidden  = dzenColor "#606060" ""
-	, ppCurrent = dzenColor "#303030" "#909090" . wrap "[" "]"
-	, ppUrgent  = dzenColor "#ff0000" "" . wrap "*" "*"
-	, ppLayout  = dzenColor "#909090" ""
-	, ppTitle   = dzenColor "#909090" "" . shorten 80
+	{ ppHidden  = xmobarColor "#00FF00" ""
+	, ppCurrent = xmobarColor "#FF0000" "" . wrap "[" "]"
+	, ppUrgent  = xmobarColor "#FF0000" "" . wrap "*" "*"
+	, ppLayout  = xmobarColor "#FF0000" ""
+	, ppTitle   = xmobarColor "#00FF00" "" . shorten 80
 	, ppSep     = "<fc=#0033FF> | </fc>"
 	}
 
--- some nice colors for the prompt windows to match the dzen status bar.
+-- some nice colors for the prompt windows to match the xmobar status bar.
 myXPConfig = defaultXPConfig
-    { font     = "-*-terminus-*-*-*-*-12-*-*-*-*-*-*-u"
-	, fgColor  = "#303030"
-	, bgColor  = "#909090"
-	, bgHLight = "#606060"
-	, fgHLight = "#303030"
+    { font     = "-*-profont-*-*-*-*-12-*-*-*-*-*-*-u"
+	, fgColor  = "#00FFFF"
+	, bgColor  = "#000000"
+	, bgHLight = "#000000"
+	, fgHLight = "#FF0000"
 	, position = Top
     }
  
 --- My Theme For Tabbed layout
 myTheme = defaultTheme 
 	{ decoHeight          = 16
-	, activeColor         = "#303030"
-	, activeBorderColor   = "#909090"
+	, activeColor         = "#a6c292"
+	, activeBorderColor   = "#a6c292"
 	, activeTextColor     = "#000000"
 	, inactiveBorderColor = "#000000"
 	}
- 
+
 --LayoutHook
-myLayoutHook = avoidStruts $ onWorkspace "web" webL $
-                             onWorkspace "irc" webL $ 
-                             onWorkspace "virtual" fullL $ 
-                             onWorkspace "vid" fullL $ 
-                             onWorkspace "gimp" gimpL $ 
-                             standardLayouts
+myLayoutHook = onWorkspace "1:chat" webL $ onWorkspace "2:web" webL $ onWorkspace "6:vbox" fullL $ onWorkspace "8:vid" fullL $ onWorkspace "9:gimp" gimpL $ standardLayouts
     where
-        standardLayouts = tiled ||| reflectTiled ||| Mirror tiled ||| Full
+        standardLayouts = avoidStruts  $ (tiled ||| reflectTiled ||| Mirror tiled ||| Full)
  
         --Layouts
         tiled = smartBorders (ResizableTall 1 (2/100) (1/2) [])
@@ -153,8 +135,7 @@ myLayoutHook = avoidStruts $ onWorkspace "web" webL $
  
         --VirtualLayout
         fullL = avoidStruts $ full
- 
- 
+
 -------------------------------------------------------------------------------
 ---- Terminal --
 myTerminal :: String
@@ -171,66 +152,76 @@ myBorderWidth :: Dimension
 myBorderWidth = 1
 -- 
 myNormalBorderColor, myFocusedBorderColor :: String
-myNormalBorderColor  = "#303030"
+myNormalBorderColor  = "#333333"
 myFocusedBorderColor = "#FF0000"
 --
   
 --Workspaces
 myWorkspaces :: [WorkspaceId]
-myWorkspaces = 	map show [1..9] ++
-                [ "web"
-                , "irc"
-                , "pdf"
-                , "doc"
-                , "virtual"
-                , "vid"
-                , "gimp"]
---
- 
+myWorkspaces = ["1:chat", "2:web", "3:code", "4:pdf", "5:doc", "6:vbox", "7:games", "8:vid", "9:gimp"]
+
 -- Switch to the "web" workspace
-viewWeb = windows (W.greedyView "web") -- (0,0a)
+viewWeb = windows (W.greedyView "2:web") -- (0,0a)
 --
- 
+
 -- keys
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 
-    [ ((modMask,      xK_b),                                 sendMessage ToggleStruts) -- layouts
-    , ((modMask,      xK_c),                                  spawn "/usr/bin/oocalc") -- OpenOffice Calc
-    , ((modMask,      xK_f),                                 spawn "/usr/bin/firefox") -- firefox
-    , ((modMask,      xK_h),                                       sendMessage Shrink) -- shrink term
-    , ((modMask,      xK_k),                                        windows W.focusUp) -- prev terminal
-    , ((modMask,      xK_l),                                       sendMessage Expand) -- expand term
-    , ((modMask,      xK_m),                                    windows W.focusMaster) -- new master
-    , ((modMask,      xK_n),                                                  refresh) -- refresh
-    , ((modMask,      xK_o),                                spawn "/usr/bin/oowriter") -- OpenOffice Writer
-    , ((modMask,      xK_p),                                   shellPrompt myXPConfig) -- program launcher
-    , ((modMask,      xK_q),                                         spawn myRestart ) -- reload config
-    , ((modMask,      xK_t),                           withFocused $ windows . W.sink) -- floating layer stuff
-    , ((modMask,      xK_z),             workspacePrompt myXPConfig (windows .W.view)) -- workspace prompt
-    , ((modMask,    xK_Tab),                                      windows W.focusDown) -- next terminal
-    , ((modMask,  xK_space),                                   sendMessage NextLayout) -- layouts
-    , ((modMask,  xK_comma),                               sendMessage (IncMasterN 1)) -- increase term in master area
-    , ((modMask,  xK_Print),                         spawn "/home/scripts/scrotinput") -- screenshot
-    , ((modMask, xK_period),                            sendMessage (IncMasterN (-1))) -- decrease term in master area
-    , ((modMask, xK_Return),                             spawn $ XMonad.terminal conf) -- launching new Terminal
-	
-    , ((modMask .|. shiftMask, xK_c),                                            kill) -- kill window
-    , ((modMask .|. shiftMask, xK_h),                        sendMessage MirrorShrink) -- mirrorshrink
-    , ((modMask .|. shiftMask, xK_j),                             windows W.swapDown ) -- swap down
-    , ((modMask .|. shiftMask, xK_k),                               windows W.swapUp ) -- swap up
-    , ((modMask .|. shiftMask, xK_l),                        sendMessage MirrorExpand) -- mirrorexpand
-    , ((modMask .|. shiftMask, xK_q),                       io (exitWith ExitSuccess)) -- quit
-    , ((modMask .|. shiftMask, xK_s),     spawn "/usr/bin/sudo /sbin/shutdown -h now") -- shutdown
-    , ((modMask .|. shiftMask, xK_z),  workspacePrompt myXPConfig (windows . W.shift)) -- mv term to workspace
-    , ((modMask .|. shiftMask, xK_space),          setLayout $ XMonad.layoutHook conf) -- layouts
-    , ((modMask .|. shiftMask, xK_Return),                       windows W.swapMaster) -- swap master
-    ]
-    ++
-    [((m .|. modMask, k), windows $ f i)                                               -- switch to workspace
-    | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]                            -- move term to workspace
-    , (f, m) <- [(W.greedyView, 0),  (W.shift, shiftMask)]]
-    where
-        myRestart = "for pid in `pgrep conky`; do kill -9 $pid; done && " ++           -- killall conky/dzen2 if running
-                    "for pid in `pgrep dzen2`; do kill -9 $pid; done && " ++           -- before executing reload
-                    "xmonad --recompile && xmonad --restart"
+		-- kiling programs
+		[ ((modMask , xK_Return), spawn $ XMonad.terminal conf)
+		, ((modMask .|. shiftMask, xK_c), kill)
+
+		-- opening program launcher
+		, ((modMask , xK_p), shellPrompt myXPConfig)
+
+		-- GridSelect
+		, ((modMask , xK_g), goToSelected defaultGSConfig)
+
+		-- layouts
+		, ((modMask, xK_space), sendMessage NextLayout)
+		, ((modMask .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
+		, ((modMask, xK_b), sendMessage ToggleStruts)
+
+		-- floating layer stuff
+		, ((modMask, xK_t), withFocused $ windows. W.sink)
+
+		--refresh
+		, ((modMask, xK_n), refresh)
+
+		-- focus
+		, ((modMask, xK_Tab), windows W.focusDown)
+		, ((modMask, xK_j), windows W.focusDown)
+		, ((modMask, xK_k), windows W.focusUp)
+		, ((modMask, xK_m), windows W.focusMaster)
+
+		-- swapping
+		, ((modMask .|. shiftMask, xK_Return), windows W.swapMaster)
+		, ((modMask .|. shiftMask, xK_j), windows W.swapDown)
+		, ((modMask .|. shiftMask, xK_k), windows W.swapUp)
+
+		-- increase or decrease number of windows in the master area
+		, ((modMask, xK_comma), sendMessage (IncMasterN 1))
+		, ((modMask, xK_period), sendMessage (IncMasterN (-1)))
+
+		-- resizing
+		, ((modMask, xK_h), sendMessage Shrink)
+		, ((modMask, xK_l), sendMessage Expand)
+		, ((modMask .|. shiftMask, xK_h), sendMessage MirrorShrink)
+		, ((modMask .|. shiftMask, xK_l), sendMessage MirrorExpand)
+
+		-- Programs
+    , ((modMask, xK_f), spawn "/usr/bin/firefox")
+    , ((modMask, xK_o), spawn "/usr/bin/ooffice")
+    , ((modMask, xK_Print), spawn "/home/scripts/scrotinput")
+
+		-- quit or restart
+		, ((modMask .|. shiftMask, xK_q), io (exitWith ExitSuccess))
+		, ((modMask, xK_q), restart "xmonad" True)
+		]
+		++
+		-- mod-[1..9] %! Switch to workspace N
+		-- mod-shift-[1..9]] %! Move client to workspace N
+		[((m .|. modMask, k), windows $ f i)
+				| (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+				, (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
